f8190211fe89c11a6052d00366cdbdef0d4ef122
twills <treav.wills@gmail.com>
2022-12-03 13:14:57 -0700

    styling on contact form

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -9,6 +9,7 @@ function Contact() {
   const [message, setMessage] = useState('');
   const [user, setUser] = useState('');
   const [formMessage, setFormMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
 
   const handleInputChange = (e) => {
@@ -35,7 +36,7 @@ function Contact() {
       return;
     }
 
-    alert(`Thanks for your message, ${user}!`);
+    setShowModal(true);
 
     setUser('');
     setMessage('');
@@ -45,31 +46,26 @@ function Contact() {
 
   return (
     <div>
-      <section class="text-gray-700 body-font relative">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+      {/* Form */}
+      <section class="text-gray-700">
+        <div class="container px-5 p-10 mx-auto">
+          <div class="flex flex-col text-center w-full mb-10">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
               Contact Me
             </h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-              I appreciate jokes, job leads, and opportunities to learn and improve!
-            </p>
-            <p class="text-red-600 mt-2">{formMessage}</p>
+            <p class="text-red-600">{formMessage}</p>
           </div>
-          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+          <div class="lg:w-1/2 md:w-2/3 mx-auto pt-2 border-t border-slate-300">
             <div class="flex flex-wrap -m-2">
               <div class="p-2 w-1/2">
-                <div class="relative">
                   <label for="name" class="leading-7 text-sm text-gray-600">
                     Name
                   </label>
                   <input value={user} name="user" type="user" onChange={handleInputChange} placeholder="Jo Nathan"
-                    class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    class="w-full rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
-                </div>
               </div>
               <div class="p-2 w-1/2">
-                <div class="relative">
                   <label
                     for="email"
                     class="leading-7 text-sm text-gray-600"
@@ -77,30 +73,26 @@ function Contact() {
                     Email
                   </label>
                   <input value={email} name="email" placeholder="email@something.com" type="email" onChange={handleInputChange}
-                    class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    class="w-full rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
-                </div>
               </div>
               <div class="p-2 w-full">
-                <div class="relative">
                   <label
                     for="message"
                     class="leading-7 text-sm text-gray-600"
                   >
                     Message
                   </label>
-                  <textarea value={message} name="message" type="message" onChange={handleInputChange}
-                    class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  <textarea value={message} name="message" type="message" onChange={handleInputChange} class="w-full rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" placeholder='Send jokes, job leads, or opportunities to learn and improve!'
                   ></textarea>
-                </div>
               </div>
-              <div class="p-2 w-full">
+              <div class="p-2 mb-2 w-full">
                 <button type="button" onClick={handleFormSubmit} class="flex mx-auto text-white bg-emerald-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                   Send
                 </button>
 
               </div>
-              <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+              <div class="p-2 w-full border-t border-slate-300 text-center">
                 <a class="text-indigo-500">treav.wills@gmail.com</a>
                 <p class="leading-normal my-5">
                   <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="_blank">
@@ -113,6 +105,21 @@ function Contact() {
           </div>
         </div>
       </section>
+      {/* End Form */}
+
+      {/* Modal */}
+      {showModal ? (
+        <div class="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 bg-emerald-400/10">
+          <div class="rounded-lg shadow-lg flex flex-col w-full bg-violet-50 p-5 m-10">
+            <p class="flex justify-center text-2xl md:text-3xl font=semibold">Thanks, {user}!</p>
+            <p class="flex justify-center m-5">Your message was not sent, but functionality is coming soon!</p>
+              <button type="button" onClick={() => setShowModal(false)} class="flex mx-auto text-white hover:bg-indigo-900 border-0 py-2 px-8 focus:outline-none bg-indigo-600 rounded text-lg">
+                  Close
+                </button>
+          </div>
+        </div>
+      ) : null}
+      {/* End Modal */}
     </div>
   );
 }
