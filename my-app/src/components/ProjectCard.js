3ceea89aa5609930b3e3e794eac09060626a84e9
twills <treavorwills@gmail.com>
2023-09-04 16:07:25 -0600

    projects page

--- my-app/src/components/ProjectCard.js
+++ my-app/src/components/ProjectCard.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { TbExternalLink } from "react-icons/tb";
-import { FaGithub } from 'react-icons/fa';
-
+import React from "react";
 
 export default function ProjectCard(props) {
-    return (
-        <div className="flex flex-wrap mt-1">
-            {props.projects.map(project => (
-                <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-lg m-5">
-                    <div className="md:flex">
-                        <div className="md:shrink-0">
-                            <img className="h-48 w-full object-cover md:h-full md:w-48" src={project.src} alt={project.alt}></img>
-                        </div>
-                        <div className="p-8">
-                            <div className="uppercase tracking-wide text-xl font-semibold">
-                                <a href={project.appLink} target="_blank" className="flex no-underline hover:text-violet-700 text-slate-900 text-xl font-semibold">{project.title} <TbExternalLink size="1.5em"/></a>
-                            </div>
-                            <p className="mt-2 text-slate-600 text-md">{project.description}</p>
-                            <a href={project.repo} target="_blank" className="flex align-middle mt-1 text-md leading-tight font-medium text-black no-underline hover:text-violet-700"><FaGithub size="2em"/><p className="ml-1 mt-2 hover:text-violet-700">View on Github</p></a>
-                        </div>
-                    </div>
-                </div>
-            ))}
+  return (
+    <>
+      {props.projects.map((project) => (
+        <div className="project">
+          <a className="project-title" href={project.appLink} target="_blank">
+            {project.title}
+          </a>
+          <a className="project-image" href={project.appLink} target="_blank">
+            <img src={project.src} alt={project.alt}></img>
+          </a>
+          <p>{project.description}</p>
+          <p className="project-links">Visit <a href={project.appLink} target="_blank">{project.title}</a> or see the code on <a href={project.repo} target="_blank">GitHub</a></p>
+          <hr className="project-hr" />
         </div>
-    );
+      ))}
+    </>
+  );
 }
--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -4,14 +4,15 @@ function Home() {
   return (
     <>
       <div className="intro-text">
-        <h1 className="name">Treavor Wills</h1>
-        <p className="description">
+        <h1>Treavor Wills</h1>
+        <p>
           web developer in Salt Lake City, Utah
         </p>
       </div>
       <picture>
         <img className="main-photo" srcSet={process.env.PUBLIC_URL + '/images/tvc.png'}/>
       </picture>
+        <p className="intro-text">I'm the one with the beard.</p>
     </>
   );
 }
