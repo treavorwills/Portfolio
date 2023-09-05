b786922fbfbf05b05b5ce98416f66d913de8f1bc
twills <treavorwills@gmail.com>
2023-09-05 00:49:35 -0600

    interactive description

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiOutlineReload } from "react-icons/ai";
 
 function Home() {
+  const [description, setDescription] = useState('in white.');
+  const [index, setIndex] = useState(0);
+
+  const descriptions = ['with the beard.', 'wearing the orange helmet.', 'between my two kids.', 'who is old.'];
+
+  const incrementArray = () => {
+    setIndex(index + 1);
+  }
+
+  useEffect(() => {
+    setDescription(descriptions[(index % descriptions.length)]);
+  }, [index]);
+
   return (
     <>
       <div className="intro-text">
@@ -12,7 +26,7 @@ function Home() {
       <picture>
         <img className="main-photo" srcSet={process.env.PUBLIC_URL + '/images/tvc.png'}/>
       </picture>
-        <p className="intro-text">I'm the one with the beard.</p>
+        <p className="sub-text" >I'm the one {description} <button className="changeDescriptionBtn" onClick={incrementArray}><AiOutlineReload></AiOutlineReload></button> </p> 
     </>
   );
 }
