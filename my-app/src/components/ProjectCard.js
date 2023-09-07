7af63e1b6c15f6f7741a360c52d506ee9afab810
twills <treavorwills@gmail.com>
2023-09-06 23:31:15 -0600

    projects - responsive

--- my-app/src/components/ProjectCard.js
+++ my-app/src/components/ProjectCard.js
@@ -8,8 +8,8 @@ export default function ProjectCard(props) {
           <a className="project-title" href={project.appLink} target="_blank">
             {project.title}
           </a>
-          <a className="project-image" href={project.appLink} target="_blank">
-            <img src={project.src} alt={project.alt}></img>
+          <a className="project-photo-anchor" href={project.appLink} target="_blank">
+            <img className="project-photo" src={project.src} alt={project.alt}></img>
           </a>
           <p>{project.description}</p>
           <p className="project-links">Visit <a href={project.appLink} target="_blank">{project.title}</a> or see the code on <a href={project.repo} target="_blank">GitHub</a></p>
