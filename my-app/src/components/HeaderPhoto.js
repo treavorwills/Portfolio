7b69dd1dd4ef1683e3ada78717ff63fddf7ff0c8
twills <treavorwills@gmail.com>
2023-09-09 14:04:31 -0600

    header

--- my-app/src/components/HeaderPhoto.js
+++ my-app/src/components/HeaderPhoto.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 export default function HeaderPhoto() {
-    return (
-        <div className="mx-auto flex mt-10 mb-5">
-            <img src={process.env.PUBLIC_URL + '/images/fall.png'} className="h-48 rounded-full w-full object-cover h-20 w-20"></img>
-            <div className="flex justify-center bg-violet-50 my-auto mx-5">
-                <p className="text-4xl font-bold text-slate-800">Treavor Wills</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <img src={process.env.PUBLIC_URL + "/images/tvc2.png"} className="header-photo"></img>
+      <p className="header-name">Treavor Wills</p>
+    </div>
+  );
 }
