4cc42ff7dc2a26818e3cb6dc56a299c4db1f73c5
twills <treavorwills@gmail.com>
2023-09-11 01:32:14 -0600

    contact page

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
 import HeaderPhoto from '../HeaderPhoto';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFillEnvelopeFill } from "react-icons/bs";
 
 
 export default function Contact() {
   return (
     <>
-    contact
+    <HeaderPhoto page='Contact'></HeaderPhoto>
+    <section className="contact">
+      <p>Hey! If anything on here caught your eye and you feel like reaching out, please do.</p>
+      <section className="tags">
+        <a className="icon-link" href="https://www.linkedin.com/in/treavorwills/" target="_blank"> <FaLinkedin size="10rem" title="Linkedin icon link to Treavor's Linkedin profile"/></a>
+        <a className="icon-link" href="mailto:treavor.wills@gmail.com" target="_blank"> <BsFillEnvelopeFill size="10rem" title="Envelope icon to send Treavor and email"/></a>
+      </section>
+    </section>
     </>
   )
 }
